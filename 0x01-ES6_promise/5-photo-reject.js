df83628626d6d6bc242118fcbe7b258a7d5df8bc
patpatmw <95435407+patpatmw@users.noreply.github.com>
2023-09-26 17:31:31 +0200

    Reject the promises

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,6 @@
+export default function uploadPhoto(filename) {
+  // This is the main function, 'uploadPhoto', which is exported as the default export of this module.
+
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+  // It returns a Promise that is immediately rejected with an error. The error message is constructed using the provided 'filename' variable, indicating that the photo cannot be processed.
+}
